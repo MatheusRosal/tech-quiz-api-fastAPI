98e0783c88032f0e9d64967e750610fe9bac3fa0
MatheusRosal <matheus@ideapadGAMING3i.localdomain>
2026-08-06 00:09:47 -0300

    adiciona serviço de llm mockado

---
+++ services/llm_service.py
@@ -0,0 +1,7 @@
+
+
+
+def generate_question_with_llm(topic: str, level: str) -> str:
+    question = f"Pergunta generica sobre {topic} em um nivel {level}"
+
+    return question
--- services/question_service.py
+++ services/question_service.py
@@ -1,8 +1,8 @@
-def generate_question(topic: str, level: str):
-    question = f"Pergunta fake sobre {topic}"
-    topic = topic
-    level = level
+from services.llm_service import generate_question_with_llm
 
+
+def generate_question(topic: str, level: str):
+    question = generate_question_with_llm(topic, level)
     return {
         "question": question,
         "topic": topic,
